71175fd8cc57b9cb493317e51fe327caaab19f4e
charan8095181918 <60030130+charan8095181918@users.noreply.github.com>
2022-04-04 12:09:25 +0530

    Add files via upload

---
+++ srff.v
@@ -0,0 +1,41 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+//  
+// Design Name: 
+// Module Name:    srff 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module srff(s,r,clk,q,qbar);
+input s,r,clk;
+output reg q,qbar;
+always @(posedge clk)
+begin
+if(s==1)
+begin
+q=1;
+qbar=0;
+end
+else if(r==1)
+begin
+q=0;
+qbar=1;
+end
+else if(s==0 & r==0)
+begin
+q<=q;
+qbar<=qbar;
+end
+end
+endmodule
---
+++ srff_test.v
@@ -0,0 +1,43 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Design Name:   srff
+// Module Name:   D:/exp/dsd/Charan/srff_test.v
+// Project Name:  Charan
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: srff
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module srff_test();
+reg s,r,clk=0;
+wire q,qbar;
+
+srff DUT(.s(s),.r(r),.clk(clk),.q(q),.qbar(qbar));
+
+initial begin
+        $monitor($time,"s=%b r=%b q=%b qbar=%b ",s,r,q,qbar);
+
+//forever 
+
+#10 s=1; r=0;
+#10 s=0;r=0;
+#10 s=0;r=1;
+#10 s=0;r=0;
+#10 s=1;r=1;
+#10;
+end
+always #5 clk=~clk;
+endmodule
